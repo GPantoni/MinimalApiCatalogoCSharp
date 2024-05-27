d395637a33cad75987960b9cdd8e3ae07fd7f4dd
Glauco Pantoni <glaucopantoni@gmail.com>
2024-05-27 17:44:10 -0300

    feat: adiciona os endpoints para Produto

--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -15,8 +15,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Catálogo de Produtos");
+app.MapGet("/", () => "Catálogo de Produtos").ExcludeFromDescription();
 
+//endpoints para categorias
 app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
 {
     db.Categorias.Add(categoria);
@@ -30,6 +31,81 @@ app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
 {
     var categoria = await db.Categorias.FindAsync(id);
     return categoria is null ? Results.NotFound() : Results.Ok(categoria);
+});
+
+app.MapPut("/categorias/{id:int}", async (int id, Categoria categoria, AppDbContext db) =>
+{
+    if (id != categoria.CategoriaId)
+        return Results.BadRequest();
+
+    var categoriaDb = await db.Categorias.FindAsync(id);
+
+    if (categoriaDb is null)
+        return Results.NotFound();
+
+    categoriaDb.Nome = categoria.Nome;
+    categoriaDb.Descricao = categoria.Descricao;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/categorias/{id:int}", async (int id, AppDbContext db) =>
+{
+    var categoria = await db.Categorias.FindAsync(id);
+
+    if (categoria is null)
+        return Results.NotFound();
+
+    db.Categorias.Remove(categoria);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+//endpoints para produtos
+app.MapPost("/produtos", async (Produto produto, AppDbContext db) =>
+{
+    db.Produtos.Add(produto);
+    await db.SaveChangesAsync();
+    return Results.Created($"/produtos/{produto.ProdutoId}", produto);
+});
+
+app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync());
+
+app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db) =>
+{
+    var produto = await db.Produtos.FindAsync(id);
+    return produto is null ? Results.NotFound() : Results.Ok(produto);
+});
+
+app.MapPut("/produtos/{id:int}", async (int id, Produto produto, AppDbContext db) =>
+{
+    if (id != produto.ProdutoId)
+        return Results.BadRequest();
+
+    var produtoDb = await db.Produtos.FindAsync(id);
+
+    if (produtoDb is null)
+        return Results.NotFound();
+
+    produtoDb.Nome = produto.Nome;
+    produtoDb.Descricao = produto.Descricao;
+    produtoDb.Imagem = produto.Imagem;
+    produtoDb.Preco = produto.Preco;
+    produtoDb.CategoriaId = produto.CategoriaId;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/produtos/{id:int}", async (int id, AppDbContext db) =>
+{
+    var produto = await db.Produtos.FindAsync(id);
+
+    if (produto is null)
+        return Results.NotFound();
+
+    db.Produtos.Remove(produto);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
 });
 
 // Configure the HTTP request pipeline.
