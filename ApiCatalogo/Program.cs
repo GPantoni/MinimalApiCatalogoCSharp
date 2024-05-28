8d528339627da9adbb51bc2b970f47073c963ec8
Glauco Pantoni <glaucopantoni@gmail.com>
2024-05-27 21:09:07 -0300

    feat: adiciona autenticação através de token jwt

--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -1,23 +1,91 @@
+using System.Text;
 using ApiCatalogo.Context;
 using ApiCatalogo.Models;
+using ApiCatalogo.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiCatalogo", Version = "v1" });
+    
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Insira o token JWT desta maneira: Bearer {seu token}"
+    });
+    
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
+builder.Services.AddSingleton<ITokenService>(new TokenService());
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Catálogo de Produtos").ExcludeFromDescription();
+//endpoint para login
+app.MapPost("/login", [AllowAnonymousAttribute](UserModel userModel, ITokenService tokenService) =>
+    {
+        if (userModel == null)
+            return Results.BadRequest("Login Inválido");
+
+        if (userModel.UserName == "glauco" && userModel.Password == "Numsey#123")
+        {
+            var tokenString = tokenService.GerarToken(app.Configuration["Jwt:Key"], app.Configuration["Jwt:Issuer"],
+                app.Configuration["Jwt:Audience"], userModel);
+            return Results.Ok(new { Token = tokenString });
+        }
+
+        return Results.BadRequest("Login Inválido");
+    }).Produces(StatusCodes.Status400BadRequest).Produces(StatusCodes.Status200OK).WithName("Login")
+    .WithTags("Autenticação");
 
 //endpoints para categorias
+app.MapGet("/", () => "Catálogo de Produtos").ExcludeFromDescription();
+
 app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
 {
     db.Categorias.Add(categoria);
@@ -25,7 +93,7 @@ app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
     return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
 });
 
-app.MapGet("/categorias", async (AppDbContext db) => await db.Categorias.ToListAsync());
+app.MapGet("/categorias", async (AppDbContext db) => await db.Categorias.ToListAsync()).WithTags("Categorias").RequireAuthorization();
 
 app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
 {
@@ -69,7 +137,7 @@ app.MapPost("/produtos", async (Produto produto, AppDbContext db) =>
     return Results.Created($"/produtos/{produto.ProdutoId}", produto);
 });
 
-app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync());
+app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync()).WithTags("Produtos").RequireAuthorization();
 
 app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db) =>
 {
@@ -116,6 +184,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
 
 app.Run();
